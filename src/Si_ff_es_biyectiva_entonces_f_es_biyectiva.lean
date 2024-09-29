1ef7886e63ca3463046a84ca17835d6fc3b36b42
Jose A. Alonso <JoseA.Alonso@gmail.com>
2024-09-29 17:56:05 +0200

    Actualización de Mathlib

--- src/Si_ff_es_biyectiva_entonces_f_es_biyectiva.lean
+++ src/Si_ff_es_biyectiva_entonces_f_es_biyectiva.lean
@@ -8,6 +8,28 @@
 -- Demostrar que si f ∘ f es biyectiva, entonces f es biyectiva.
 -- ---------------------------------------------------------------------
 
+-- Demostración en lenguaje natural
+-- ================================
+
+-- Es consecuencia de los siguientes lemas (demostrados en ejercicios
+-- anteriores):
+-- + Lema 1: Si g ∘ f es inyectiva. entonces f es inyectiva.
+-- + Lema 2: Si g ∘ f es suprayectiva. entonces g es suprayectiva.
+-- En efecto, supongamos que
+--    f ∘ f es biyectiva
+-- entonces se tiene que
+--    f ∘ f es inyectiva                                             (1)
+--    f ∘ f es suprayectiva                                          (2)
+-- De (1) y el Lema 1, se tiene que
+--    f es inyectiva                                                 (3)
+-- De (2) y el Lema 2, se tiene que
+--    f es suprayectiva                                              (4)
+-- De (3) y (4) se tiene que
+--    f es biyectiva
+
+-- Demostraciones con Lean4
+-- ========================
+
 import Mathlib.Tactic
 open Function
 
@@ -23,6 +45,31 @@ example
   (h : Bijective (f ∘ f))
   : Bijective f :=
 by
+  have h1 : Injective (f ∘ f) := Bijective.injective h
+  have h2 : Surjective (f ∘ f) := Bijective.surjective h
+  have h3 : Injective f := Injective.of_comp h1
+  have h4 : Surjective f := Surjective.of_comp h2
+  show Bijective f
+  exact ⟨h3, h4⟩
+
+-- 2ª demostración
+-- ===============
+
+example
+  (f : X → X)
+  (h : Bijective (f ∘ f))
+  : Bijective f :=
+⟨Injective.of_comp (Bijective.injective h),
+ Surjective.of_comp (Bijective.surjective h)⟩
+
+-- 3ª demostración
+-- ===============
+
+example
+  (f : X → X)
+  (h : Bijective (f ∘ f))
+  : Bijective f :=
+by
   constructor
   . -- ⊢ Injective f
     have h1 : Injective (f ∘ f) := Bijective.injective h
@@ -30,3 +77,9 @@ by
   . -- ⊢ Surjective f
     have h2 : Surjective (f ∘ f) := Bijective.surjective h
     exact Surjective.of_comp h2
+
+-- Lemas usados
+-- ============
+
+-- #check (Injective.of_comp : Injective (g ∘ f) → Injective f)
+-- #check (Surjective.of_comp : Surjective (g ∘ f) → Surjective g)
